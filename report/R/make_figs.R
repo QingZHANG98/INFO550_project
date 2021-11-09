77c3c438a37543614e00c9288cb204a94210c485
qing <qzha284@emory.edu>
2021-11-09 17:38:58 -0500

    remove KMplot

--- report/R/make_figs.R
+++ report/R/make_figs.R
@@ -21,72 +21,74 @@ png(filename = "./report/figs/corrplot.png")
 corrplot::corrplot(cor(corp))
 dev.off()
 
-
+####### there are some bugs to execute kmplot in windows system
 ################## KM-plot #######
-require(survminer)
+
+########## code ############
+# require(survminer)
 require(ggplot2)
 
-if(!require(survival)){
-    install.packages('survival')
-    require(survival)
-  }
+# if(!require(survival)){
+#     install.packages('survival')
+#     require(survival)
+#   }
 
-# grouped by quantile
-data <- data %>% 
-  mutate(AgeGroup = as.numeric(Age >= quantile(Age)[2]) +
-           as.numeric(Age >= quantile(Age)[3])+
-           as.numeric(Age >= quantile(Age)[4]),
-         Pro4Group = as.numeric(Protein4 >= 0.5) 
-)
+# # grouped by quantile
+# data <- data %>% 
+#   mutate(AgeGroup = as.numeric(Age >= quantile(Age)[2]) +
+#            as.numeric(Age >= quantile(Age)[3])+
+#            as.numeric(Age >= quantile(Age)[4]),
+#          Pro4Group = as.numeric(Protein4 >= 0.5) 
+# )
 
-KM_fit <- survfit(Surv(survdays,censor) ~ Pro4Group, data)
-png(filename = "./report/figs/KM_fig1.png")
-ggsurvplot(KM_fit,data,
-           risk.table = T,
-           pval = T,
-           conf.int = T,
-           xlab = "Time in days",
-           legend.labs = c('standard Protein4 < 0.5','standard Protein4 >= 0.5')
-) +
-labs(title = "KM est grouped by protein 4")
-dev.off()
-# Because the sample size is small. we can see the trend of difference in survival proba, but it's not significant.
+# KM_fit <- survfit(Surv(survdays,censor) ~ Pro4Group, data)
+# png(filename = "./report/figs/KM_fig1.png")
+# ggsurvplot(KM_fit,data,
+#            risk.table = T,
+#            pval = T,
+#            conf.int = T,
+#            xlab = "Time in days",
+#            legend.labs = c('standard Protein4 < 0.5','standard Protein4 >= 0.5')
+# ) +
+# labs(title = "KM est grouped by protein 4")
+# dev.off()
+# # Because the sample size is small. we can see the trend of difference in survival proba, but it's not significant.
 
-## surv ~ tumor stage
-KM_fit2 <- survfit(Surv(survdays,censor) ~ Tumour_Stage , data)
-png(filename = "./report/figs/KM_fig2.png")
-ggsurvplot(KM_fit2,data,
-           #risk.table.col = "Surgery type",
-           #risk.table = T,
-           pval = T,
-           conf.int = T,
-           xlab = "Time in days"
-)+
-labs(title = "KM est grouped by Tumour Stage")
-dev.off()
+# ## surv ~ tumor stage
+# KM_fit2 <- survfit(Surv(survdays,censor) ~ Tumour_Stage , data)
+# png(filename = "./report/figs/KM_fig2.png")
+# ggsurvplot(KM_fit2,data,
+#            #risk.table.col = "Surgery type",
+#            #risk.table = T,
+#            pval = T,
+#            conf.int = T,
+#            xlab = "Time in days"
+# )+
+# labs(title = "KM est grouped by Tumour Stage")
+# dev.off()
 
-## Histology
-KM_fit3 <- survfit(Surv(survdays,censor) ~ Histology , data)
-png(filename = "./report/figs/KM_fig3.png")
-ggsurvplot(KM_fit3,data,
-           pval = T,
-           conf.int = T,
-           xlab = "Time in days"
-)+
-labs(title = "KM est grouped by Histology")
-dev.off()
+# ## Histology
+# KM_fit3 <- survfit(Surv(survdays,censor) ~ Histology , data)
+# png(filename = "./report/figs/KM_fig3.png")
+# ggsurvplot(KM_fit3,data,
+#            pval = T,
+#            conf.int = T,
+#            xlab = "Time in days"
+# )+
+# labs(title = "KM est grouped by Histology")
+# dev.off()
 
-# these lines are crossed, the log-rank test cannot find out the difference
-KM_fit4 <- survfit(Surv(survdays,censor) ~ Histology , 
-                   Surv1Year %>% filter(Histology %in% c('Infiltrating Ductal Carcinoma','Infiltrating Lobular Carcinoma')))
-png(filename = "./report/figs/KM_fig4.png")
-ggsurvplot(KM_fit4,Surv1Year,
-           pval = T,
-           conf.int = T,
-           xlab = "Time in days"
-)+
-labs(title = "The first year KM est grouped by Histology")
-dev.off()
+# # these lines are crossed, the log-rank test cannot find out the difference
+# KM_fit4 <- survfit(Surv(survdays,censor) ~ Histology , 
+#                    Surv1Year %>% filter(Histology %in% c('Infiltrating Ductal Carcinoma','Infiltrating Lobular Carcinoma')))
+# png(filename = "./report/figs/KM_fig4.png")
+# ggsurvplot(KM_fit4,Surv1Year,
+#            pval = T,
+#            conf.int = T,
+#            xlab = "Time in days"
+# )+
+# labs(title = "The first year KM est grouped by Histology")
+# dev.off()
 
 ##### objective 2 
 table_stage <- data.frame(table(data$Surgery_type,data$Tumour_Stage))
